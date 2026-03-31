06066fe637c1c71569aeaeb908d5150562d9b4b0
amrxtgh <ghimireamrit619@gmail.com>
2026-04-01 01:07:37 +0545

    only the running part

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
-use core::panic;
+use tiny_assembler::assemble_line;
 
 fn main() {
-    let input = fs::read_to_string("input.s").expect("failed to read file.");
+    let input = fs::read_to_string("input.s").unwrap();
 
     for line in input.lines() {
         if line.trim().is_empty() {
@@ -13,43 +13,6 @@ fn main() {
     }
 }
 
-fn assemble_line(line: &str) -> u32 {
-    let clean = line.replace(",", "");
 
-    let tokens: Vec<&str> = clean.split_whitespace().collect();
-    if tokens.len() != 4 { panic!("invalid instruction format"); }
 
-    let op = tokens[0];
-    match op {
-        "add" => encode_r(0b0000000, reg(tokens[3]), reg(tokens[2]), 0b000, reg(tokens[1]), 0b0110011),
-        "sub" => encode_r(0b0100000, reg(tokens[3]), reg(tokens[2]), 0b000, reg(tokens[1]), 0b0110011),
-        "and" => encode_r(0b0000000, reg(tokens[3]), reg(tokens[2]), 0b111, reg(tokens[1]), 0b0110011),
-        "or"  => encode_r(0b0000000, reg(tokens[3]), reg(tokens[2]), 0b110, reg(tokens[1]), 0b0110011),
-        _ => panic!("unknown instruction"),
-    }
-}
-
-fn reg(r: &str) -> u32 {
-    if let Some(num) = r.strip_prefix('x') {
-        num.parse::<u32>().unwrap()
-    } else {
-        panic!("invalid register");
-    }
-}
-
-fn encode_r(
-    func7: u32,
-    rs2: u32,
-    rs1: u32,
-    func3: u32,
-    rd: u32,
-    opcode: u32,
-    ) -> u32 {
-    (func7 << 25)
-        | (rs2 << 20)
-        | (rs1 << 15)
-        | (func3 << 12)
-        | (rd << 7)
-        | opcode
-}
 
