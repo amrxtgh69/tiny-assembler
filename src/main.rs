b70b30ec8d0aa92c2c890bc5a6747db3469c63f9
amrxtgh <ghimireamrit619@gmail.com>
2026-04-21 22:27:58 +0545

    now migrate that into the rvcli lets do this shi

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::fs;
+use std::{fs::{self, File}, io::Write};
 use tiny_assembler::assemble_line;
 
 fn main() {
     let input = fs::read_to_string("input.s").unwrap();
 
+    let mut file = File::create("program.bin").unwrap();
     for line in input.lines() {
         if line.trim().is_empty() {
             continue;
         }
         let instr = assemble_line(line);
-        println!("{:#010x}", instr); 
+        // println!("{:#010x}", instr); 
+        
+        // i love rust straight to little endian bytes hehe :))
+        let bytes = instr.to_le_bytes();
+        file.write_all(&bytes).unwrap();
     }
 }
 
